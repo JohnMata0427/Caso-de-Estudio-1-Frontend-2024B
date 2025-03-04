0c1b72e0d9d6eb15c6e7de602a729f164b75e77d
JohnMata0427 <jhonmata0427@gmail.com>
2025-03-03 20:42:58 -0500

    Add Vercel analytics and speed insights packages to improve performance monitoring

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
+
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
@@ -8,3 +10,4 @@ bootstrapApplication(AppComponent, appConfig).catch((err) =>
 );
 
 inject();
+injectSpeedInsights();
